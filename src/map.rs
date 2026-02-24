64ddde6e083af3d1475a37da50bc290acb675aea
Abel Tesfa <abeltesfa198@gmail.com>
2026-02-24 11:35:25 +0300

    feat: Added get_val and set_weighted_val for WsSink

--- src/map.rs
+++ src/map.rs
@@ -33,8 +33,6 @@ where
 {
     pub fn get_val(&self, path: &[u8]) -> Option<H> {
         use pathmap::zipper::{Zipper, ZipperCreation};
-        // self.inner is Arc<ZipperHeadOwned<H>>. ZipperHeadOwned implements ZipperCreation.
-        // read_zipper_at_path returns Result<ReadZipperTracked...>.
         match self.inner.read_zipper_at_path(path) {
             Ok(z) => z.val().cloned(),
             Err(_) => None,
@@ -43,7 +41,6 @@ where
 
     pub fn set_weighted_val(&self, path: &[u8], val: H) -> Result<(), ()> {
         use pathmap::zipper::{Zipper, ZipperCreation, ZipperWriting};
-        // write_zipper_at_exclusive_path returns Result<WriteZipperTracked...>
         if let Ok(mut z) = self.inner.write_zipper_at_exclusive_path(path) {
             z.set_val(val);
             Ok(())
